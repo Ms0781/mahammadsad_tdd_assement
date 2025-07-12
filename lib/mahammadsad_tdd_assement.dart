12718bfd4c13cb440feb9daa7ed164c62bb699cd
ms_igex <mahammadsad@igexsolutions.in>
2025-07-12 11:02:21 +0530

    String which contain only digits : Regular Expression Added.

--- lib/mahammadsad_tdd_assement.dart
+++ lib/mahammadsad_tdd_assement.dart
@@ -1,7 +1,10 @@
 int? add(String numbers) {
-  if (numbers.isEmpty) return 0;
+  if (numbers.trim().isEmpty) return 0;
 
-   return numbers
+  if(numbers.length == 1 && RegExp(r'^\d+$').hasMatch(numbers) ) return int.tryParse(numbers);
+
+  //Allow the add method to handle any amount of numbers.
+  return numbers
         .split(',')
         .map((e) => int.tryParse(e.trim()) ?? 0)
         .reduce((a, b) => a + b);
